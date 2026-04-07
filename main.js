e49202b0de55185d935d90b5c86f0cff82603c8d
Adan-878 <itenirary@gmail.com>
2026-04-07 19:28:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ let s = document.getElementById("screen");
 function add(x) {
   if (s.innerText == "0") {
     s.innerText = x;
+  } else {
+    s.innerText += x;
   };
-  else s.innerText += x;
 }
 
 function clearScreen() {
