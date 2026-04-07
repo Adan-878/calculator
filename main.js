6aca88690abd00682f8b59a856965f0e4801f052
Adan-878 <itenirary@gmail.com>
2026-04-07 19:23:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("Welcome to my calculator");
+
+let s = document.getElementById("screen");
+
+function add(x) {
+  if (s.innerText == "0") {
+    s.innerText = x;
+  };
+  else s.innerText += x;
+}
+
+function clearScreen() {
+  s.innerText = "0";
+}
+
+function calc() {
+  s.innerText = eval(s.innerText);
+}
